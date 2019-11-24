29409d013f8a75ab7030e5bbe71b973b90395ed2
raviMukti <shinjiravi@gmail.com>
2019-11-24 22:49:52 +0700

    Penambahan Home Splash Screen

--- app/src/androidTest/java/com/aftwork/resale/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/aftwork/resale/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.aftwork.resale;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- app/src/main/java/com/aftwork/resale/MainActivity.java
+++ app/src/main/java/com/aftwork/resale/MainActivity.java
@@ -1,13 +1,32 @@
 package com.aftwork.resale;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonGet;
+    Animation uptodown, downtoup;
+    TextView textView;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonGet = (Button)findViewById(R.id.buttonGetStarted);
+        textView = (TextView)findViewById(R.id.textView);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        uptodown = AnimationUtils.loadAnimation(this, R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this, R.anim.downtoup);
+        textView.setAnimation(uptodown);
+        imageView.setAnimation(uptodown);
+        buttonGet.setAnimation(downtoup);
     }
 }
